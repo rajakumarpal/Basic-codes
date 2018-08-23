09e57468298c3675b5c9dfc319ada58711caa749
rajakumarpal <42609060+rajakumarpal@users.noreply.github.com>
2018-08-23 18:54:45 +0530

    Input output

---
+++ gfgio.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //code
+    int a,b,T,i;
+    cin>>T;
+    for(i=0;i<T;i++)
+    {
+        cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+    
+    return 0;
+}
